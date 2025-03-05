711ac109556a6316b66c095d70b74f635e8c9e3f
Diego0923 <diego288205@gmail.com>
2025-03-05 17:05:32 +0800

    refactor:  modified variable names in calculate_pi.py

--- CleanCodeHW/calculate_pi.py
+++ CleanCodeHW/calculate_pi.py
@@ -1,22 +1,22 @@
 import random
 
 # Constant settings
-CIRCLE_RADIUS = 1
-TOTAL_POINTS = 1000000
-MULTIPLIER_FOR_AREA = 4
+RADIUS = 1
+NUM_POINTS = 1000000
+AREA_FACTOR = 4
 SQUARE_EXPONENT = 2
 POINT_COUNT_INCREMENT = 1
 
 points_inside_circle = 0
 
 # Randomly generate points and count those inside the circle
-for point in range(TOTAL_POINTS):
-    x_coordinate = random.uniform(-CIRCLE_RADIUS, CIRCLE_RADIUS)
-    y_coordinate = random.uniform(-CIRCLE_RADIUS, CIRCLE_RADIUS)
-    if x_coordinate**SQUARE_EXPONENT + y_coordinate**SQUARE_EXPONENT <= CIRCLE_RADIUS**SQUARE_EXPONENT:
+for point in range(NUM_POINTS):
+    x_coordinate = random.uniform(-RADIUS, RADIUS)
+    y_coordinate = random.uniform(-RADIUS, RADIUS)
+    if x_coordinate**SQUARE_EXPONENT + y_coordinate**SQUARE_EXPONENT <= RADIUS**SQUARE_EXPONENT:
         points_inside_circle += POINT_COUNT_INCREMENT
 
 # Estimate pi based on the number of points inside the circle
-estimated_pi = (points_inside_circle / TOTAL_POINTS) * MULTIPLIER_FOR_AREA
+estimated_pi = (points_inside_circle / NUM_POINTS) * AREA_FACTOR
 
 print(f"Estimated value of pi is: {estimated_pi}")
