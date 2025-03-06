aefd839bbaf6e410f0b48232435729cb28b419ba
Diego0923 <diego288205@gmail.com>
2025-03-06 21:18:44 +0800

    fix: testing solution unittest

--- UnitTestHW/Regular Expression Matching/test_solution.py
+++ UnitTestHW/Regular Expression Matching/test_solution.py
@@ -1,8 +1,9 @@
-import ...
-from ...
+import pytest
+from solution import Solution
 
 @pytest.fixture
-...
+def solution():
+    return Solution()
     
 testcases = [
     # s, p, expected_res
@@ -15,8 +16,8 @@ testcases = [
     ["aaa", "ab*a*c*a", True]
 ]
 
-@pytest.mark.parametrize([?], testcases)
-...
+@pytest.mark.parametrize("s, p, expected_res", testcases)
+def test_isMatch(solution, s, p, expected_res):
     assert solution.isMatch(s, p) == expected
 
 """
