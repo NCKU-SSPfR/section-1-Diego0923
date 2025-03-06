6236784a57d906b05e07385253bafc83c4788e25
Diego0923 <diego288205@gmail.com>
2025-03-06 20:40:14 +0800

    fix: testing basic unittest in test_fizzbuzz.py

--- UnitTestHW/Fizz Buzz/Basic/test_fizzbuzz.py
+++ UnitTestHW/Fizz Buzz/Basic/test_fizzbuzz.py
@@ -7,7 +7,7 @@ def test_5():
     assert fizzBuzz(5) == "Buzz"
 
 def test_15():
-    assert fizzBuzz(15) == "Fizz" + "Buzz"
+    assert fizzBuzz(15) == "FizzBuzz"
 
 def test_2():
-    assert fizzBuzz(2) == f"{str(n)}"
+    assert fizzBuzz(2) == "2"
