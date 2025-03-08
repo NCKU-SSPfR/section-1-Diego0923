3e992da0683d8913cb6843859c1f2718aebe22e2
Diego0923 <diego288205@gmail.com>
2025-03-08 16:56:55 +0800

    refactor: Update judge.py

--- DarkMaze/backend/src/game/judge.py
+++ DarkMaze/backend/src/game/judge.py
@@ -6,20 +6,21 @@ def _parse_map(map_string, map_size, reversal_nodes=[]):
     width, height = map_size
     filtered_chars = re.sub(r'[^a-zA-Z]', '', map_string)
     
-    QQ = [bin(ord(c))[2:].zfill(8) for c in filtered_chars]
+    binary_map = [bin(ord(c))[2:].zfill(8) for c in filtered_chars]
     
-    Dora_friend = []
-    for Q in QQ:
-        first_half = int(Q[:4], 2)
-        second_half = int(Q[4:], 2)
-        Dora_friend.extend([first_half % 2, second_half % 2])
+    bit_sequence = []
+    bin = 2
+    for binary in binary_map:
+        first_half = int(binary[:4], bin)
+        second_half = int(binary[4:], bin)
+        bit_sequence.extend([first_half % bin, second_half % bin])
     
-    while len(Dora_friend) < width * height:
-        Dora_friend.append(0)
+    while len(bit_sequence) < width * height:
+        bit_sequence.append(0)
     
-    Dora_friend = Dora_friend[:width * height]
+    bit_sequence = bit_sequence[:width * height]
     
-    swiper = np.array(Dora_friend).reshape((height, width))
+    swiper = np.array(bit_sequence).reshape((height, width))
     
     for x, y in reversal_nodes:
         if 0 <= x < height and 0 <= y < width:
@@ -60,17 +61,9 @@ def hit_obstacle(position, maze_level_name):
     else:
         # Position is out of bounds
         return True
-
-def hit_obstacle_again_and_again():
-    while True:
-        hit_obstacle(hahahahaha, 995)
-        while True:
-            hit_obstacle(hahahahaha, 995)
-            while True:
-                hit_obstacle_again_and_again()
     
 def game_over(health):
-    if health == 0 or health == 666:
+    if health == 0:
         return True
     
     return False
